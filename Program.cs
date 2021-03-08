67d5a0505851340196d882114d5d57a53af7a0db
Erik Isaksen <72092585+Erikisaksen93@users.noreply.github.com>
2021-03-08 12:51:35 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroduksjonTilObjekter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
